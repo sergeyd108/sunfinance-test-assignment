b0523c358f80146c8b111c6644f2bb27bb704cef
Sergey Dorovsky <sergey.dorovsky@drm.la>
2026-05-22 21:42:14 +0300

    initial setup

---
+++ app/app.vue
@@ -0,0 +1,58 @@
+<script setup>
+useHead({
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  htmlAttrs: { lang: 'en' },
+})
+
+const title = 'Weather App'
+const description = 'Powerful Weather App for Sun Finance test assignment.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="h-6 w-auto shrink-0" />
+        </NuxtLink>
+
+        <TemplateMenu />
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+
+        <UButton
+          to="https://github.com/nuxt-ui-templates/starter"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="i-lucide-sun" />
+
+    <UFooter>
+      <template #left>
+        <p class="text-muted text-sm">Weather App • © {{ new Date().getFullYear() }}</p>
+      </template>
+    </UFooter>
+  </UApp>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // ssr: false,
+
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt', '@pinia/nuxt'],
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-01-15',
+
+  runtimeConfig: {
+    openWeatherAppId: '',
+  },
+
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        plugins: [{ name: '@vue/typescript-plugin' }],
+      },
+    },
+  },
+})
